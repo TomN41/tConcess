3ee973f652526cf18d917fef72d79aafac4c8638
TomN <98286538+TomN41@users.noreply.github.com>
2022-01-24 01:39:08 +0100

    Add files via upload

---
+++ tConcess/config.lua
@@ -0,0 +1,25 @@
+Concess             = {}
+
+Concess.webhooks = "TON WEEBHOOK ICI"
+
+Concess.jeveuxmarker = true --- true = Oui | false = Non
+
+Concess.jeveuxblips = true --- true = Oui | false = Non
+
+Concess.pos = {
+	menu = {
+		position = {x = -56.7, y = -1098.77, z = 26.42}
+	},
+	boss = {
+		position = {x = -29.91, y = -1107.04, z = 26.42}
+	},
+	serrurier = {
+		position = {x = -50.23, y = -1089.45, z = 26.42}
+	},
+	coffre = {
+		position = {x = -30.66, y = -1111.06, z = 26.42}
+	},
+	spawnvoiture = {
+		position = {x = -51.38, y = -1094.05, z = 26.42, h = 251.959}
+	},
+}
